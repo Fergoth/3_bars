9d28f22f827448fff47b21f94067968fb4bb1f32
Fergoth <fergothew@gmail.com>
2019-04-09 11:29:45 +0300

    рефакторинг

--- bars.py
+++ bars.py
@@ -4,15 +4,18 @@ from math import radians, cos, sin, asin, sqrt
 
 
 def load_data(filepath):
-    with open(filepath, encoding='utf-8') as file:
-        data_from_file = json.load(file)
-        return data_from_file['features']
-
+    try:
+        with open(filepath, encoding='utf-8') as file:
+            data_from_file = json.load(file)
+            return data_from_file['features']
+    except FileNotFoundError:
+        print('Файл не найден')
+    except json.decoder.JSONDecodeError:
+        print('Файл содержит данные не в формате json')
 
 def get_biggest_bar(bars):
     biggest_bar = max(bars, key=lambda x: x['properties']['Attributes']['SeatsCount'])
-    print('Самый большой бар')
-    print_bar(biggest_bar)
+    return biggest_bar
 
 
 def get_smallest_bar(bars):
@@ -45,18 +48,19 @@ def print_bar(bar):
 if __name__ == '__main__':
     try:
         path = sys.argv[1]
-        json_data = load_data(path)
-        print('Введите текущие координаты через пробел')
-        lon, lat = map(float, input().split())
     except IndexError:
         print('Требуется путь к файлу как аргумент')
-    except FileNotFoundError:
-        print('Файл не найден')
-    except json.decoder.JSONDecodeError:
-        print('Файл содержит данные не в формате json')
-    except ValueError:
-        print('Введите два числа в формате float через пробел \n Например 37.454 32.4353')
-    else:
+        sys.exit(0)
+    json_data = load_data(path)
+    if json_data:
+        while 1:
+            print('Введите текущие координаты через пробел')
+            try:
+                lon, lat = map(float, input().split())
+            except ValueError:
+                print('Введите два числа в формате float через пробел \n Например 37.454 32.4353')
+            else:
+                break
         print('Ближайший бар:')
         print_bar(get_closest_bar(json_data, lon, lat))
         print('Самый большой бар:')
